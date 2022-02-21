44876220c924a031de9d31bf8a8848786765f503
BGMP <jose@bgmp.cl>
2022-02-21 02:54:41 -0300

    Tweak versioning

--- data.rb
+++ data.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
-NOW = Time.now
-
 module RVATracks
   NAME = 'rva_tracks'
   DESCRIPTION = "Re-Volt America's Tracks Pack"
-  YEAR = "#{NOW.year.digits[0]}#{NOW.year.digits[1]}"
-  MONTH = NOW.month < 10 ? "0#{NOW.month}" : NOW.month.to_s
-  DAY = NOW.day < 10 ? "0#{NOW.day}" : NOW.day.to_s
-  VERSION = "#{YEAR}.#{MONTH}#{DAY}"
-  URL = 'https://distribute.revolt-america.com/rva/rva_tracks.zip'
+  YEAR = 22
+  MONTH = 2
+  DAY = 21
+  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}"
 end
