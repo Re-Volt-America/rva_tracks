dc01287239ee707883d7977b5246925d8bf62031
BGMP <jose@bgmp.cl>
2022-03-21 00:38:37 -0300

    Update version format

--- data.rb
+++ data.rb
@@ -6,6 +6,6 @@ module RVATracks
   YEAR = 22
   MONTH = 2
   DAY = 21
-  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY < 10 ? "0#{DAY}" : DAY}"
+  VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}"
   URL = 'https://distribute.revolt-america.com/rva/rva_tracks.zip'
 end
