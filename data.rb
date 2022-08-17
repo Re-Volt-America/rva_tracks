460363925120c427c042ce7efcc9e1e7c1dab7cd
unknown <pichubritos@gmail.com>
2022-08-17 19:41:32 -0300

    Track revisions made by MightyCucumber, Zeino & BGM

--- data.rb
+++ data.rb
@@ -4,9 +4,9 @@ module RVATracks
   NAME = 'rva_tracks'
   DESCRIPTION = "Re-Volt America's Tracks Pack"
   YEAR = 22
-  MONTH = 5
-  DAY = 9
-  REVISION = 2
+  MONTH = 8
+  DAY = 17
+  REVISION = 1
   SUFFIX = 'a'
   VERSION = "#{YEAR}.#{MONTH < 10 ? "0#{MONTH}" : MONTH}#{DAY}#{SUFFIX}-#{REVISION}"
   URL = 'https://distribute.revolt-america.com/rva/rva_tracks.zip'
